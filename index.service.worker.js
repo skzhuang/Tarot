e4729013e8409db77ac87cc99db3fc45a5b66b1b
skzhuang <121989163+skzhuang@users.noreply.github.com>
2025-04-04 08:45:49 -0700

    update

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1743690143|212494335';
+const CACHE_VERSION = '1743781513|13045401833';
 /** @type {string} */
 const CACHE_PREFIX = 'Tarot-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
